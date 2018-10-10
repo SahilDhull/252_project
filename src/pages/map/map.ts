aedef30864db598c14c698b440a116183cdc7bf5
SahilDhull <sahildhull25121998@gmail.com>
2018-10-10 12:22:34 +0530

    first commit

---
+++ src/pages/map/map.ts
@@ -0,0 +1,83 @@
+import { Component , ViewChild, ElementRef} from '@angular/core';
+import { IonicPage, LoadingController, NavController, NavParams } from 'ionic-angular';
+import 'leaflet';
+import { Geolocation } from '@ionic-native/geolocation';
+import L from 'leaflet';
+
+/**
+ * Generated class for the MapPage page.
+ *
+ * See https://ionicframework.com/docs/components/#navigation for more info on
+ * Ionic pages and navigation.
+ */
+
+@IonicPage()
+@Component({
+  selector: 'page-map',
+  templateUrl: 'map.html',
+})
+export class MapPage {
+  @ViewChild('map') mapContainer: ElementRef;
+  map: any;
+
+  loading;
+  constructor(private loadingCtrl: LoadingController, public navCtrl: NavController, public navParams: NavParams, private gl: Geolocation) {
+    /**
+     * Map takes a bit to load once the page is opened. 
+     * So we show a loading spinner to the user until the map is completely loaded.
+     * We use Ionic's loading controller. Link: https://ionicframework.com/docs/api/components/loading/LoadingController/
+     */
+    this.presentLoading();
+  }
+
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad MapPage');
+    var map  = L.map("map").fitWorld();
+
+    /**
+     * Apparently you don't need any tokens to use the maps. 
+     */
+    L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attributions: 'www.tphangout.com',
+      maxZoom: 18 
+    }).addTo(map);
+
+    map.locate({
+      setView: true, 
+      maxZoom: 17
+    })
+    .on('locationfound', (e) => {
+      this.loading.dismiss(); //Stop showing the loading spinner once the map is loaded.
+      const { lat, lng } = e.target.getCenter();
+      let markerGroup = L.featureGroup();
+      let marker: any = L.marker([lat,lng]).on('click',() => {
+        alert("marker clicked");
+      })
+      markerGroup.addLayer(marker);
+      map.addLayer(markerGroup);
+      }).on('locationerror', (err) => {
+        alert("error");
+    })
+  }
+
+  presentLoading() {
+    this.loading = this.loadingCtrl.create({
+      content: 'Loading map...'
+    });
+
+    this.loading.present();
+  }
+}
+
+
+/*
+let markerGroup = L.featureGroup();
+      let marker: any = L.marker([e.latitude, e.longitude]).on('click', () => {
+        alert('Marker clicked');
+      })
+      markerGroup.addLayer(marker);
+      map.addLayer(markerGroup);
+      }).on('locationerror', (err) => {
+        alert(err.message);
+    })
+*/
